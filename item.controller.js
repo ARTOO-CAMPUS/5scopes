db73e93835f67d22c6929cd8bd962d23fb1ccdd5
paolocalvaresi <paolo.calvaresi.v@gmail.com>
2017-03-01 17:55:37 +0100

    soluzione

--- item.controller.js
+++ item.controller.js
@@ -1,6 +1,7 @@
 // CREA UN CONTROLLER PER L'OGGETTO
 angular.module('app').controller('ItemCtrl', function ($scope) {
 
-	$scope.label = "Nome"
+	$scope.label = "Nome",
+	$scope.saluta ="Ciao";
 
 })
